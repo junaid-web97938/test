8db45559a699ef71f5f12303c3c8896da0ef7bff
junaid-web97938 <junaid-web97@outlook.com>
2026-01-25 21:32:14 -0600

    assignmentfix

--- modules/usermodule/testmodule/backend.tf
+++ modules/usermodule/testmodule/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "1resourcegroup"
-    storage_account_name = "2ndstorageacct"
-    container_name       = "tfstate"
-    key                  = "entra-users.tfstate"
+    resource_group_name  = "ResourceGroup1"
+    storage_account_name = "storageaccount1"
+    container_name       = "scontainer1"
+    key                  = "multiple-users.tfstate"
   }
 }
