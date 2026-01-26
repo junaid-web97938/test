121c2a3be8a2f05a8bd45f8d70ec48eb98cdbf52
junaid-web97938 <junaid-web97@outlook.com>
2026-01-25 22:24:53 -0600

    a

--- modules/usermodule/testmodule/backend.tf
+++ modules/usermodule/testmodule/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "ResourceGroup1"
     storage_account_name = "storageaccount2cat"
     container_name       = "scontainer1"
-    key                  = "multiple-users.tfstate"
+    key                  = "entra-users.tfstate"
   }
 }
