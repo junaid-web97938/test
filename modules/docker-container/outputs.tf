e83262487ba22ff65192042cf8d6a5fd918f7325
junaid-web97938 <junaid-web97@outlook.com>
2026-02-05 01:13:19 -0600

    new

--- modules/docker-container/outputs.tf
+++ modules/docker-container/outputs.tf
@@ -1,11 +1,11 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "container_group_name" {
   value = azurerm_container_group.aci.name
 }
 
 output "container_fqdn" {
   value = azurerm_container_group.aci.fqdn
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
--- modules/docker-container/variables.tf
+++ modules/docker-container/variables.tf
@@ -1,9 +1,41 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "container_group_name" { type = string }
-variable "container_name" { type = string }
-variable "container_image" { type = string }
-variable "container_cpu" { type = number, default = 1 }
-variable "container_memory" { type = number, default = 1.5 }
-variable "container_port" { type = number, default = 80 }
-variable "dns_name_label" { type = string }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "container_group_name" {
+  type = string
+}
+
+variable "container_name" {
+  type    = string
+  default = "scontainer1"
+}
+
+variable "container_image" {
+  type    = string
+  default = "ubuntu"
+}
+
+variable "container_cpu" {
+  type    = number
+  default = 1
+}
+
+variable "container_memory" {
+  type    = number
+  default = 1.5
+}
+
+variable "container_port" {
+  type    = number
+  default = 80
+}
+
+variable "dns_name_label" {
+  type    = string
+  default = "scontainer1"
+}
