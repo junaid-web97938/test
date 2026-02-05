37dc7d776afca640c6a8912693b8e5f69a3124e2
junaid-web97938 <junaid-web97@outlook.com>
2026-02-05 01:16:48 -0600

    new

--- modules/docker-container/backend.tf
+++ modules/docker-container/backend.tf
@@ -5,9 +5,5 @@ terraform {
     container_name       = "tfstate"
     key                  = "aci-container.terraform.tfstate"
 
-    # backend SP credentials read from env
-    client_id       = getenv("ARM_CLIENT_ID")
-    tenant_id       = getenv("ARM_TENANT_ID")
-    subscription_id = getenv("ARM_SUBSCRIPTION_ID")
   }
 }
